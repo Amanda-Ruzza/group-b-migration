d8c90cd76c605212659858b6e605c099cec8c536
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2023-06-11 12:00:04 -0400

    Update Providers.tf + Add AWS Backend

--- backends.tf
+++ backends.tf
@@ -1,1 +1,10 @@
 # --- root/backends.tf --
+
+terraform {
+  backend "s3" {
+    bucket = "migrationav"
+    key    = "group-b/tf-state/terraform.tfstates"
+    region = "us-east-1"
+    dynamodb_table = "migavtable"
+  }
+}
