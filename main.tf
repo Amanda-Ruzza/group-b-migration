e37bbe71d4b37d0d0f4cc500343f6737a7df4bef
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2023-06-20 16:29:19 -0400

    Add 'locals.tf' to the main.tf with VPC CIDR

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 # --- root/main.tf --
 
+
 module "networking" {
   source   = "./networking"
-  vpc_cidr = "10.0.0.0/16"
+  # vpc_cidr = "10.0.0.0/16"
+  vpc_cidr = local.vpc_cidr
   public_sn_count = var.public_sn_count
   private_sn_count = var.private_sn_count
   max_subnets = var.max_subnets
